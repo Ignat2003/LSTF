19996ab86b754b45d8a3a2338b6fa27e569c7176
Ignat2003 <ignat.gotin@gmail.com>
2024-07-03 17:24:36 +0300

    delete classification metric

--- Informer/exp/exp_informer.py
+++ Informer/exp/exp_informer.py
@@ -198,18 +198,22 @@ class Exp_Informer(Exp_Basic):
             print("Epoch: {} cost time: {}".format(epoch+1, time.time()-epoch_time))
             train_loss = np.average(train_loss)
             vali_loss, all_trues, all_preds = self.vali(vali_data, vali_loader, criterion)
-            all_preds = np.minimum(np.maximum(all_preds, 0), 1)
-            roc_auc_val = roc_auc_score(all_trues, all_preds)
-            f1_val = f1_score(all_trues, np.round(all_preds))
+            
+            # all_preds = np.minimum(np.maximum(all_preds, 0), 1)
+            # roc_auc_val = roc_auc_score(all_trues, all_preds)
+            # f1_val = f1_score(all_trues, np.round(all_preds))
+            
             test_loss, all_trues, all_preds = self.vali(test_data, test_loader, criterion)
-            all_preds = np.minimum(np.maximum(all_preds, 0), 1)
+            # all_preds = np.minimum(np.maximum(all_preds, 0), 1)
             # roc_auc_test = roc_auc_score(all_trues, all_preds)
             # f1_test = f1_score(all_trues, np.round(all_preds))
             roc_auc_test, f1_test = 0,0
             print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f} Test Loss: {4:.7f}".format(
                 epoch + 1, train_steps, train_loss, vali_loss, test_loss))
-            print('Val Roc auc: {0:.3f} | Test Roc auc: {1:.3f}'.format(roc_auc_val, roc_auc_test))
-            print('Val F1: {0:.3f} | Test F1: {1:.3f}'.format(f1_val, f1_test))
+            
+            
+            # print('Val Roc auc: {0:.3f} | Test Roc auc: {1:.3f}'.format(roc_auc_val, roc_auc_test))
+            # print('Val F1: {0:.3f} | Test F1: {1:.3f}'.format(f1_val, f1_test))
             
             early_stopping(vali_loss, self.model, path)
             if early_stopping.early_stop:
